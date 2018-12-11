c7201d809bf279be82dd9dde430086a93b34f691
Justice Fears <justice.fears@gmail.com>
2018-12-11 15:10:47 -0600

    Created main method

--- Practice.java
+++ Practice.java
@@ -1,6 +1,8 @@
-package GitProjects.practicepushing
+package GitProjects.practicepushing;
 
 public class Practice{
+    public static void main(String[] args){
     System.out.println("Justice is the author");
     System.out.println("She made a change");
+    }
 }
